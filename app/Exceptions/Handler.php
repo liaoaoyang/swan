f79b46efa5011e3fecb891c51b4eaacf99b09de7
liaoaoyang <liaoaoyang@gmail.com>
2017-10-12 22:20:10 +0800

    Return json when client expect or requested send API

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -14,14 +14,15 @@ class Handler extends ExceptionHandler
      *
      * @var array
      */
-    protected $dontReport = [
-        \Illuminate\Auth\AuthenticationException::class,
-        \Illuminate\Auth\Access\AuthorizationException::class,
-        \Symfony\Component\HttpKernel\Exception\HttpException::class,
-        \Illuminate\Database\Eloquent\ModelNotFoundException::class,
-        \Illuminate\Session\TokenMismatchException::class,
-        \Illuminate\Validation\ValidationException::class,
-    ];
+    protected $dontReport
+        = [
+            \Illuminate\Auth\AuthenticationException::class,
+            \Illuminate\Auth\Access\AuthorizationException::class,
+            \Symfony\Component\HttpKernel\Exception\HttpException::class,
+            \Illuminate\Database\Eloquent\ModelNotFoundException::class,
+            \Illuminate\Session\TokenMismatchException::class,
+            \Illuminate\Validation\ValidationException::class,
+        ];
 
     /**
      * Report or log an exception.
@@ -49,27 +50,24 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        $exceptionData = [
+            'exception' => [
+                'desc' => '其他错误',
+            ],
+        ];
+
         if ($exception instanceof \EasyWeChat\Core\Exceptions\HttpException) {
             if (strpos($exception->getMessage(), 'require subscribe hint') !== false) {
                 Log::error(__METHOD__ . '|EasyWeChat Exception: ' . $exception->getMessage());
-                return response()->view('swan/exception', [
-                    'exception' => [
-                        'desc' => '未关注公众号',
-                    ]
-                ]);
+                $exceptionData['exception']['desc'] = '未关注公众号';
             }
         }
 
-        if (!config('app.debug')) {
-            Log::error(__METHOD__ . '|Exception: ' . $exception->getMessage());
-            return response()->view('swan/exception', [
-                'exception' => [
-                    'desc' => $exception->getMessage(),
-                ]
-            ]);
+        if ($request->expectsJson() || preg_match('#^/wechat/swan/\S+\.send$#', $request->getPathInfo())) {
+            return response()->json($exceptionData);
         }
 
-        return parent::render($request, $exception);
+        return response()->view('swan/exception', $exceptionData);
     }
 
     /**
