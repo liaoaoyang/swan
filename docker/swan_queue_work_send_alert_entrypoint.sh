8a8e4c8f4ab33774fe81485891526839ba6c14e4
liaoaoyang <liaoaoyang@gmail.com>
2019-03-06 22:15:47 +0800

    Use relative directory in entrypoint.sh

--- docker/swan_queue_work_send_alert_entrypoint.sh
+++ docker/swan_queue_work_send_alert_entrypoint.sh
@@ -12,6 +12,13 @@ if [ $? -ne 0 ]; then
     exit
 fi
 
+BASE_DIR=$(cd `dirname $0`; pwd)
+BASE_DIR=`dirname $BASE_DIR`
 
+if [ ! -f $BASE_DIR/artisan ];then
+    exit
+fi
 
-/usr/local/bin/php /var/www/html/artisan queue:work redis --queue=$SWAN_ASYNC_SEND_QUEUE
+cd $BASE_DIR
+
+/usr/local/bin/php $BASE_DIR/artisan queue:work redis --queue=$SWAN_ASYNC_SEND_QUEUE
